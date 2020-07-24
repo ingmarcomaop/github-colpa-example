f728427b5a7a4b7556d3bb799aae955bcded3d95
Marco <ing.marcomaop@gmail.com>
2020-07-24 09:52:37 -0500

    sql presentation

--- sql-files/sql-query.sql
+++ sql-files/sql-query.sql
@@ -1,5 +1,5 @@
 INSERT INTO dbo.Customers
    ([CustomerId],[Name],[Location],[Email])
 VALUES
-   ( 10, N'New 5 folder', N'United 5 folder', N'new5@works5.com')
+   ( 12, N'New 12 folder', N'United 12 folder', N'new5@works5.com')
 GO
